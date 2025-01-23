34039b93f7ebecefc955cfcbf4feb0e6ae3b4644
SmoothMonologue <mobugy99@gmail.com>
2025-01-23 19:24:17 +0900

    add: 보드 엔티티

--- src/boards/entities/board.entity.ts
+++ src/boards/entities/board.entity.ts
@@ -1,1 +1,36 @@
-export class Board {}
+import { IsString } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'Board',
+})
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int', { nullable: false })
+  userId: number;
+
+  @Column('enum', { default: false })
+  visibility: Enumerator;
+
+  @IsString()
+  @Column('varchar', {})
+  color: String;
+
+  @IsString()
+  @Column('varchar', { length: 10, nullable: false })
+  title: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
