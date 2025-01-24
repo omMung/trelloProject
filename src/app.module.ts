75784051531c3470c278df2a340f3e2c4f0947f4
chansikjeong <cjyong1004@naver.com>
2025-01-24 14:19:28 +0900

    Updata joi 임시 주석처리

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,24 +15,23 @@ import { CardMembersModule } from './card-members/card-members.module';
 import { LabelsModule } from './labels/labels.module';
 import { CheckitemsModule } from './checkitems/checkitems.module';
 import { CardLabelsModule } from './card-labels/card-labels.module';
-import {Alarm} from "./alarms/entities/alarm.entity";
-import {Board} from "./boards/entities/board.entity";
-import {CardLabel} from "./card-labels/entities/card-label.entity";
-import {JoinMember} from "./card-members/entities/card-member.entity";
-import {Card} from "./cards/entities/card.entity";
-import {CheckItem} from "./checkitems/entities/checkitem.entity";
-import {CheckList} from "./checklists/entities/checklist.entity";
-import {Comment} from "./comments/entities/comment.entity";
-import {Label} from "./labels/entities/label.entity";
-import {List} from "./lists/entities/list.entity";
-import {Member} from "./members/entities/member.entity";
-import {User} from "./users/entities/user.entity";
-import Joi from "joi";
-
+import { Alarm } from './alarms/entities/alarm.entity';
+import { Board } from './boards/entities/board.entity';
+import { CardLabel } from './card-labels/entities/card-label.entity';
+import { JoinMember } from './card-members/entities/card-member.entity';
+import { Card } from './cards/entities/card.entity';
+import { CheckItem } from './checkitems/entities/checkitem.entity';
+import { CheckList } from './checklists/entities/checklist.entity';
+import { Comment } from './comments/entities/comment.entity';
+import { Label } from './labels/entities/label.entity';
+import { List } from './lists/entities/list.entity';
+import { Member } from './members/entities/member.entity';
+import { User } from './users/entities/user.entity';
+import Joi from 'joi';
 
 const typeOrmModuleOptions = {
   useFactory: async (
-      configService: ConfigService,
+    configService: ConfigService,
   ): Promise<TypeOrmModuleOptions> => ({
     type: 'mysql', //분리 추천??
     host: configService.get('DB_HOST'),
@@ -40,26 +39,52 @@ const typeOrmModuleOptions = {
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_NAME'),
-    entities: [Alarm,Board,CardLabel,JoinMember,Card,CheckItem,CheckList,Comment,Label,List,Member,User],
+    entities: [
+      Alarm,
+      Board,
+      CardLabel,
+      JoinMember,
+      Card,
+      CheckItem,
+      CheckList,
+      Comment,
+      Label,
+      List,
+      Member,
+      User,
+    ],
     synchronize: configService.get('DB_SYNC'),
     logging: true,
   }),
   inject: [ConfigService],
 };
 @Module({
-  imports: [ ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      DB_HOST: Joi.string().required(),
-      DB_PORT: Joi.number().required(),
-      DB_USERNAME: Joi.string().required(),
-      DB_PASSWORD: Joi.string().required(),
-      DB_NAME: Joi.string().required(),
-      DB_SYNC: Joi.boolean().required(),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // validationSchema: Joi.object({
+      //   DB_HOST: Joi.string().required(),
+      //   DB_PORT: Joi.number().required(),
+      //   DB_USERNAME: Joi.string().required(),
+      //   DB_PASSWORD: Joi.string().required(),
+      //   DB_NAME: Joi.string().required(),
+      //   DB_SYNC: Joi.boolean().required(),
+      // }),
     }),
-  }),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
-    UsersModule, BoardsModule, ListsModule, CardsModule, CommentsModule, ChecklistsModule, AlarmsModule, MembersModule, CardMembersModule, LabelsModule, CheckitemsModule, CardLabelsModule],
+    UsersModule,
+    BoardsModule,
+    ListsModule,
+    CardsModule,
+    CommentsModule,
+    ChecklistsModule,
+    AlarmsModule,
+    MembersModule,
+    CardMembersModule,
+    LabelsModule,
+    CheckitemsModule,
+    CardLabelsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
