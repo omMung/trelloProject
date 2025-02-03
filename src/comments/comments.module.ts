519f1c29c1f543cd69e3325acbd7384ba01ac8fc
KimDongHwan <kdhn712@gmail.com>
2025-02-03 15:11:18 +0900

    update: 코멘트 모듈 임포트 추가

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { JwtModule } from '@nestjs/jwt';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { Comment } from './entities/comment.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    JwtModule.register({}),
+    UsersModule,
+  ],
   controllers: [CommentsController],
-  providers: [CommentsService],
+  providers: [CommentsService, JwtAuthGuard],
 })
 export class CommentsModule {}
