b8da1b3d86df6f11d4d1602972ba5115508ed29e
KimDongHwan <kdhn712@gmail.com>
2025-02-03 21:05:03 +0900

    add: 전역 예외 에러 처리 추가했습니다.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 // import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("서버가 정상적으로 시작 되었습니다")
+  app.useGlobalFilters(new HttpExceptionFilter());
+  console.log('서버가 정상적으로 시작 되었습니다');
   await app.listen(3000);
 }
 bootstrap();
