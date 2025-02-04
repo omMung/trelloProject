0fd04caeaa596a6b050fc598713b268554fb1024
YooDaeWon <dbeodnjs1001@naver.com>
2025-02-04 16:54:15 +0900

    update ci.yml

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -6,21 +6,21 @@ import {
   Patch,
   Param,
   Delete,
-  UseGuards,
-  Request,
+  // UseGuards,
+  // Request,
 } from '@nestjs/common';
 import { ListsService } from './lists.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 import { UpdateListPositionsDto } from './dto/update-list-positions.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+// import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('lists')
 export class ListsController {
   constructor(private readonly listsService: ListsService) {}
 
   // 리스트 생성
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createListDto: CreateListDto) {
     return this.listsService.create(createListDto);
