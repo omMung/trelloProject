e51e2a30bca944893fa26908a7b034318cb23209
Omungchi <cjyong1004@naver.com>
2025-02-19 14:44:29 +0900

    Update main.ts 파이프 적용

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,26 @@ import { AppModule } from './app.module';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 요청 데이터를 DTO 속성 타입으로 자동 변환
+      whitelist: true, // DTO에 정의되지 않은 속성 제거));
+      exceptionFactory: (errors) => {
+        console.error(errors); // 오류 정보 콘솔에 출력
+        const messages = errors.map((error) =>
+          Object.values(error.constraints).join(', '),
+        );
+        return new BadRequestException({ message: '검증실패' });
+      },
+    }),
+  );
 
   app.useWebSocketAdapter(new IoAdapter(app));
 
